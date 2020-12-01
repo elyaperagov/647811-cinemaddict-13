64e47ba1adba2a23917ae08e141f3ea453a4c10f
elyaperagov <piragof@gmail.com>
2020-12-01 08:00:19 +0300

    делает небольшие правки по заданию

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import FilmCard from "./view/film-card.js";
 import ShowMoreButton from "./view/show-more.js";
 import FilmsExtra from "./view/films-extra.js";
 import PopUpFilmCard from "./view/popup.js";
-import {generateCard, generateCards} from './mock/cards.js';
+import {generateCard} from './mock/cards.js';
 import {RenderPosition, renderElement, getRandomArrayItem} from './helpers.js';
 
 const CARDS_IN_ROW = 5;
@@ -15,8 +15,6 @@ const NUMBER_OF_EXTRA_CONTAINERS = 2;
 const FILM_CARDS_QUANTITY = 15;
 
 let showingFilmCardsCount = CARDS_IN_ROW;
-
-const allFilms = generateCards(FILM_CARDS_QUANTITY);
 
 const generatedCards = new Array(FILM_CARDS_QUANTITY).fill().map(generateCard);
 
@@ -28,14 +26,14 @@ const renderCards = (cards, container) => {
 
 const favorCount = generatedCards.filter((card) => card.isFavorite).length;
 const watchedCount = generatedCards.filter((card) => card.isWatched).length;
-const isInWatchedListCount = generatedCards.filter((card) => card.isInWatchedList).length;
+const isInWatchListCount = generatedCards.filter((card) => card.isInWatchList).length;
 
 const siteHeaderElement = document.querySelector(`.header`);
 const siteMainElement = document.querySelector(`.main`);
 const siteBody = document.querySelector(`body`);
 
 renderElement(siteHeaderElement, new Profile().getElement(), RenderPosition.BEFOREEND);
-renderElement(siteMainElement, new Navigation(isInWatchedListCount, watchedCount, favorCount).getElement(), RenderPosition.BEFOREEND);
+renderElement(siteMainElement, new Navigation(isInWatchListCount, watchedCount, favorCount).getElement(), RenderPosition.BEFOREEND);
 renderElement(siteMainElement, new Sort().getElement(), RenderPosition.BEFOREEND);
 renderElement(siteMainElement, new Films().getElement(), RenderPosition.BEFOREEND);
 
@@ -57,7 +55,7 @@ const showMoreCards = () => {
 
   showingFilmCardsCount += CARDS_IN_ROW;
 
-  if (showingFilmCardsCount > allFilms.length) {
+  if (showingFilmCardsCount > generatedCards.length) {
     loadMoreButton.remove();
   }
 };
@@ -74,9 +72,49 @@ for (let i = 0; i < NUMBER_OF_EXTRA_CONTAINERS; i++) {
 const topRated = document.querySelector(`.films-list--extra .films-list__container`);
 const mostCommented = document.querySelector(`.films-list--extra:last-child .films-list__container`);
 
+
+const getTopRatedFilms = () => {
+  const topRatedFilms = generatedCards
+  .sort(function (a, b) {
+    return a.rating - b.rating;
+  }).slice(generatedCards.length - 2, generatedCards.length);
+  return topRatedFilms;
+};
+
+const filmTitles = siteFilmsList.querySelectorAll(`.film-card__title`);
+const filmPosters = siteFilmsList.querySelectorAll(`.film-card__poster`);
+const filmComments = siteFilmsList.querySelectorAll(`.film-card__comments`);
+
+const closePopup = (evt) => {
+  evt.preventDefault();
+  // for (var i = 0; i < generatedCards.length; i++) {
+  new PopUpFilmCard().getElement().remove();
+  // debugger;
+  // }
+};
+
+const openPopup = (evt) => {
+  evt.preventDefault();
+  // for (var i = 0; i < generatedCards.length; i++) {
+  renderElement(siteBody, new PopUpFilmCard(generatedCards[0]).getElement(), RenderPosition.BEFOREEND);
+  const closePopupBtn = document.querySelector(`.film-details__close-btn`);
+  closePopupBtn.addEventListener(`click`, closePopup);
+  // }
+};
+
+const renderPopup = (elements) => {
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener(`click`, openPopup);
+  }
+};
+
+renderPopup(filmTitles);
+renderPopup(filmPosters);
+renderPopup(filmComments);
+
 for (let i = 0; i < NUMBER_OF_EXTRA_CONTAINERS; i++) {
-  renderElement(topRated, new FilmCard(getRandomArrayItem(generatedCards)).getElement(), RenderPosition.BEFOREEND);
+  renderElement(topRated, new FilmCard(getRandomArrayItem(getTopRatedFilms())).getElement(), RenderPosition.BEFOREEND);
   renderElement(mostCommented, new FilmCard(getRandomArrayItem(generatedCards)).getElement(), RenderPosition.BEFOREEND);
 }
 
-renderElement(siteBody, new PopUpFilmCard(generatedCards[0]).getElement(), RenderPosition.BEFOREEND);
+// renderElement(siteBody, new PopUpFilmCard(generatedCards[0]).getElement(), RenderPosition.BEFOREEND);
--- src/mock/cards.js
+++ src/mock/cards.js
@@ -6,15 +6,15 @@ const RATING_DIVIDER = 10;
 const generateCard = () => {
   let card = {
     title: getRandomArrayItem(FILM_TITLES),
-    description: shuffleArray(descriptions).slice(0, getRandomInteger(0, DESCRIPTION_MAX)).join(` `),
+    description: shuffleArray(descriptions).slice(0, getRandomInteger(1, DESCRIPTION_MAX)).join(` `),
     duration: getTimeFromMins(getRandomInteger(DURATION.MIN, DURATION.MAX)),
     genre: getRandomArrayItem(genres),
     comments: getRandomInteger(COMMENTS.MIN, COMMENTS.MAX),
     year: (randomDate(new Date(YEARS.MIN, 0, 1))).getFullYear(),
     rating: (getRandomInteger(RATING.MIN, RATING.MAX) / RATING_DIVIDER).toFixed(1),
-    isInWatchList: Boolean(getRandomInteger(0, 1)),
-    isWatched: Boolean(getRandomInteger(0, 1)),
-    isFavorite: Boolean(getRandomInteger(0, 1)),
+    isInWatchList: Boolean(getRandomInteger()),
+    isWatched: Boolean(getRandomInteger()),
+    isFavorite: Boolean(getRandomInteger()),
   };
   return card;
 };
